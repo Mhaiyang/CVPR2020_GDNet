fd79299981c22bf3fccc0b1ccbb0acd8a232d82a
mhaiyang <mhy666@mail.dlut.edu.cn>
2020-03-15 18:54:29 +0800

    TaylorMei 2020.03.15

---
+++ config.py
@@ -0,0 +1,15 @@
+"""
+ @Time    : 2020/3/15 18:44
+ @Author  : TaylorMei
+ @E-mail  : mhy666@mail.dlut.edu.cn
+ 
+ @Project : CVPR2020_GDNet
+ @File    : config.py
+ @Function:
+ 
+"""
+backbone_path = '/home/iccd/CVPR2020_GDNet/backbone/resnext/resnext_101_32x4d.pth'
+
+gdd_training_root = "/home/iccd/CVPR2020_GDNet/GDD/train"
+gdd_testing_root = "/home/iccd/CVPR2020_GDNet/GDD/test"
+gdd_results_root = "/home/iccd/CVPR2020_GDNet/results"
